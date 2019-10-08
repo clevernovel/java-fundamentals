088b26109a3eb6fa1b2674ca3c8869c0d94dcd92
cleve <clevernovel@gmail.com>
2019-10-08 19:04:00 +0300

    task3 added

--- intro/src/main/java/com/epam/courses/java/fundamentals/intro/practice/task3/Table.java
+++ intro/src/main/java/com/epam/courses/java/fundamentals/intro/practice/task3/Table.java
@@ -1,6 +1,9 @@
 package com.epam.courses.java.fundamentals.intro.practice.task3;
 
+import java.util.Arrays;
+
 import static java.lang.Math.tan;
+import static java.lang.System.out;
 
 public class Table {
 
@@ -11,7 +14,7 @@ public class Table {
   public Table(double start, double end, double step) {
 
     if (end <= start)
-      throw new RuntimeException("End value shoult be bigger then start, but it's not.");
+      throw new RuntimeException("End value should be bigger than start, but it's not.");
 
     this.start = start;
     this.end = end;
@@ -27,8 +30,27 @@ public class Table {
   }
 
   public double[][] getTable() {
-
-    //todo реализовать
     return new double[0][0];
+  }
+
+  public static double[][] getTable(double start, double end, double step) {
+    int size = 0;
+    for (double temp = start; temp <= end; temp += step) {
+      size++;
+    }
+    double[][] table = new double[size][2];
+    double x = start;
+    for (int i = 0; i < table.length; i++) {
+      table[i][0] = x;
+      table[i][1] = function(x);
+      out.println(" " + Arrays.deepToString(table) + " ");
+      out.println();
+      x += step;
+    }
+    return table;
+  }
+
+  public static void main(String... __) {
+    getTable(1, 4, 0.5);
   }
 }
